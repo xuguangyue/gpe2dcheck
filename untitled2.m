1d836cdb3e6fa35d66526c408ab08c224ce610a1
xuguangyue <xgyue1984@gmail.com>
2018-12-06 10:11:55 +0800

    new parameters

--- untitled2.m
+++ untitled2.m
@@ -3,7 +3,7 @@ clc
 
 ff = './';
 
-for j=16
+for j=44
     
     if j==0        
         file = [ff,'real2d-den-init'];
@@ -12,8 +12,12 @@ for j=16
     end
     
     [x,y,psi]=loadxy(file);
-    surf(x,y,abs(psi).^2)
+    surf(x,y,log10(abs(psi).^2))
     shading interp 
     colormap(hot)
+    view(0,90)
+    axis equal
+    xlim([-32 32])
+    ylim([-32 32])
     
 end
